6ccbd4d49813f776cbc59937b28e3e95c94f3e68
Charles-Emmanuel <charlesemmanuel.bray@gmail.com>
2021-05-15 14:02:32 +0200

    add sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './containers/Home';
 
