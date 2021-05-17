839c06cc1142618dddee360418973238191386d8
GuardYard <fathi.kadir.a@gmail.com>
2021-05-17 18:34:46 +0200

    add post methode

--- src/helpers/UserHelpers.tsx
+++ src/helpers/UserHelpers.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import axios from "axios";
 
-const ALL_USER_API_BASE_URL = "http://localhost:8080/api/v1/users"
+const ALL_USER_API_BASE_URL = "http://localhost:8080/api/user"
+
 
 class UserHelpers{
     getAllUsers(){
-        return axios.get(ALL_USER_API_BASE_URL);
+        return axios.get(ALL_USER_API_BASE_URL + "/users");
     }
+
+    createUser(user: any){
+        return axios.post(ALL_USER_API_BASE_URL + "/addUser", user)
+    }
+
+
 }
 
 export default new UserHelpers()
