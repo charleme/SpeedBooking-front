91cd8e6659d7cb2a253885080a9f53622d5acb46
GuardYard <fathi.kadir.a@gmail.com>
2021-05-17 19:43:11 +0200

    add update helper

--- src/helpers/UserHelpers.tsx
+++ src/helpers/UserHelpers.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import axios from "axios";
 
-const ALL_USER_API_BASE_URL = "http://localhost:8080/api/user"
+const USER_API_BASE_URL = "http://localhost:8080/api/user"
 
 
 class UserHelpers{
     getAllUsers(){
-        return axios.get(ALL_USER_API_BASE_URL + "/users");
+        return axios.get(USER_API_BASE_URL + "/users");
     }
 
     createUser(user: any){
-        return axios.post(ALL_USER_API_BASE_URL + "/addUser", user)
+        return axios.post(USER_API_BASE_URL + "/addUser", user)
     }
 
+    getUserById(userId: number){
+        return axios.get(USER_API_BASE_URL + "/findUser/" + userId)
+    }
+
+    updateUser(user: any, userId: number){
+        return axios.put(USER_API_BASE_URL + "/updateUser/" + userId, user)
+    }
 
 }
 
