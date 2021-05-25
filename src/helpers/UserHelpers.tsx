a730efb0e60579cf861afdd8bc2af5ea8c80d562
Charles-Emmanuel <charlesemmanuel.bray@gmail.com>
2021-05-25 10:08:43 +0200

    create some helpers

--- src/helpers/UserHelpers.tsx
+++ src/helpers/UserHelpers.tsx
@@ -11,19 +11,23 @@ class UserHelpers{
     }
 
     createUser(user: IUser): Promise<AxiosResponse<any>>{
-        return axios.post(USER_API_BASE_URL + "/addUser", user)
+        return axios.post(USER_API_BASE_URL + "/addUser", user);
     }
 
     getUserById(userId: number): Promise<AxiosResponse<IUser>>{
-        return axios.get(USER_API_BASE_URL + "/findUser/" + userId)
+        return axios.get(USER_API_BASE_URL + "/findUser/" + userId);
     }
 
     updateUser(user: IUser, userId: number): Promise<AxiosResponse<any>>{
-        return axios.put(USER_API_BASE_URL + "/updateUser/" + userId, user)
+        return axios.put(USER_API_BASE_URL + "/updateUser/" + userId, user);
     }
 
     deleteUser(userId: number): Promise<AxiosResponse<any>>{
-        return axios.delete(USER_API_BASE_URL + "/deleteUser/" + userId)
+        return axios.delete(USER_API_BASE_URL + "/deleteUser/" + userId);
+    }
+
+    getUserReadBooks(userId:number): Promise<AxiosResponse<any>>{
+        return axios.get(USER_API_BASE_URL + "/getUserReadBooks/" + userId);
     }
 }
 
