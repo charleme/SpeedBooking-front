65d312fd45be9962e9c39a352337be82db5f5575
Charles-Emmanuel <charlesemmanuel.bray@gmail.com>
2021-05-25 11:30:39 +0200

    update progress

---
+++ src/helpers/UserBookHelpers.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import axios, { AxiosResponse } from "axios";
+import IUser from "../data_interface/IUser";
+
+const USER_BOOK_API_BASE_URL = "http://localhost:8080/api/userbook"
+
+const GET_USER_BOOK_PROGRESS = "/getUserBookProgress/";
+class UserBookHelpers{
+    getProgress(idUser: number, idBook: number): Promise<AxiosResponse<number>>{
+        return axios.get(USER_BOOK_API_BASE_URL + GET_USER_BOOK_PROGRESS + idUser + "&" + idBook);
+    }
+
+    updateProgress(idUser: number, idBook: number, progressValue: number): Promise<AxiosResponse<any>>{
+        return axios.post(USER_BOOK_API_BASE_URL + "/updateProgress/" + idUser + "&" + idBook + "&" + progressValue);
+    }
+}
+
+export default new UserBookHelpers()
