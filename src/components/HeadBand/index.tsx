8b507b79db6b6bc145409604516957e25d552550
Fathi <fathi.kadir.a@gmail.com>
2021-06-01 03:40:16 +0200

    waiting for fixing

--- src/components/HeadBand/index.tsx
+++ src/components/HeadBand/index.tsx
@@ -5,90 +5,67 @@ import {colors} from "../../default_color";
 import UserHelpers from "../../helpers/UserHelpers";
 import IUser from "../../data_interface/IUser"
 
-interface IState {
-    user: IUser;
-    currentUserId: number | null
-}
-
-export default class HeadBand extends React.Component<any, IState>{
-
-    constructor(props: any) {
-        super(props);
-
-        this.state = {
-            currentUserId: null,
-            user: {
-                username: '',
-                email: '',
-                password: '',
-                languages: '',
-            },
-        }
-    }
-    componentDidMount() {
-        this.setState({currentUserId: (localStorage.getItem("id") !== null) ? Number(localStorage.getItem("id")) : null})
-        if(this.state.currentUserId !== null) {
-            UserHelpers.getUserById(this.state.currentUserId).then((response) => {
-                this.setState({user: response.data})
-                console.log(this.state.user)
-            })
-        }
-    }
+export default class HeadBand extends React.Component{
 
     render() {
-        const contentUnlogged = <Grid style={{height:'100%',  width:'330px'}}
-                                      container
-                                      direction="row"
-                                      justify="space-between"
-                                      alignItems="center">
-                                    <Button variant="contained"
-                                            size="large" color="primary"
-                                            href="http://localhost:3000/signin"
-                                            style={{backgroundColor:colors.orangeButton, color:colors.white}}>
-                                        Se connecter
-                                    </Button>
-                                    <Button variant="outlined"
-                                            size="large"
-                                            href="http://localhost:3000/signup"
-                                            style={{borderColor:colors.orangeButton, color:colors.orangeButton}}>
-                                        S'inscrire
-                                    </Button>
-                                </Grid>
+        let contentUnlogged;
+        const currentUserId:number|null  = (localStorage.getItem("id") !== null) ? Number(localStorage.getItem("id")) : null ;
+        console.log(currentUserId)
+        if(currentUserId === null){
+            contentUnlogged = <Grid
+                style={{height:'100%',  width:'330px'}}
+                container
+                direction="row"
+                justify="space-between"
+                alignItems="center"
+            >
+                <Button variant="contained"
+                        size="large" color="primary"
+                        href="http://localhost:3000/signin"
+                        style={{backgroundColor:colors.orangeButton, color:colors.white}}>
+                    Se connecter
+                </Button>
+                <Button variant="outlined"
+                        size="large"
+                        href="http://localhost:3000/signup"
+                        style={{borderColor:colors.orangeButton, color:colors.orangeButton}}>
+                    S'inscrire
+                </Button>
+            </Grid>
+        }else{
+            UserHelpers.getUserById(currentUserId).then((response) => {
+                const user:IUser = response.data
+                var randomColor = "#".concat(Math.floor(user.username.charCodeAt(0)*16777215).toString(16))
 
-        let content
-        var randomColor = "#".concat(Math.floor(this.state.user.username.charCodeAt(0)*16777215).toString(16))
-        console.log(Math.random()*16777215)
+                contentUnlogged = <Grid style={{height:'100%',  width:'330px'}}
+                                        container
+                                        direction="row"
+                                        justify="center"
+                                        alignItems="center">
+                    <Avatar style={{backgroundColor:randomColor, color:"white", marginRight:"3%"}}>
+                        {user.username.substring(0,1)}
+                    </Avatar>
+                    <Link to="/profile" style={{textDecoration:"none"}}>
+                        <h4>{user.username}</h4>
+                    </Link>
+                </Grid>
+            })
 
-        if(this.state.currentUserId !== null) {
-            content = <Grid style={{height:'100%',  width:'330px'}}
-                            container
-                            direction="row"
-                            justify="center"
-                            alignItems="center">
-                        <Avatar style={{backgroundColor:randomColor, color:"white", marginRight:"3%"}}>
-                            {this.state.user.username.substring(0,1)}
-                        </Avatar>
-                        <Link to="/profile" style={{textDecoration:"none"}}>
-                            <h4>{this.state.user.username}</h4>
-                        </Link>
-                      </Grid>
-        }else {
-            content = contentUnlogged
         }
 
         return (
-                <Grid
-                    style={{height:'9.99%', padding:'0 1.5em 0 0.8em', backgroundColor:colors.white}}
-                    container
-                    direction="row"
-                    justify="space-between"
-                    alignItems="center"
-                >
-                    <Link to="/">
-                        <img src={"logo.png"}  alt="SpeedBooking"/>
-                    </Link>
-                    {content}
-                </Grid>
+            <Grid
+                style={{height:'9.99%', padding:'0 1.5em 0 0.8em', backgroundColor:colors.white}}
+                container
+                direction="row"
+                justify="space-between"
+                alignItems="center"
+            >
+                <Link to="/">
+                    <img src={"logo.png"}  alt="SpeedBooking"/>
+                </Link>
+                {contentUnlogged}
+            </Grid>
         );
     }
 }
